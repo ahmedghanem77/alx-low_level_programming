5df318f2960c6bc285e9d79e5450cdede2e63fcf
ahmedghanem77 <ahmed@example.com>
2023-07-13 16:30:27 -0700

    as

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,12 @@
 /**
  * main -Entry point
  *
- *Description:A C program that prints with function 
+ * Description: A C program that prints with put function 
  *
  * return: Always 0 (success)
- *
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+        return(0);
 }
