442489f4839476b9835bd48e2a034c6a154a8f0c
ahmedghanem77 <ahmed@example.com>
2023-07-14 19:14:12 -0700

    as

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,22 +3,21 @@
 /**
  * main - Entry point
  *
- * Description: Prints the size of various types on the computer it is compiled and run on.
+ * Description: Prints the size of various types on the computer it is 
  *
  * Return: Always 0 (success)
 */
 int main(void)
 {
-	char ch = 'a'; 
+	char ch = 'a';
 	char CH = 'A';
 
 	while (ch <= 'z')
 	{
 		putchar (ch);
 		ch++;
-	}		
-	
-	while (CH <='Z')
+	}	
+	while (CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
